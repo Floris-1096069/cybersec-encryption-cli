9d5a48ecf2576e09729935ef8d4c94a584cd3498
Floris-1096069 <1096069@hr.nl>
2025-10-24 22:43:58 +0200

    Featre, simple CLI app for encrypting with aes-128

---
+++ encrypt.py
@@ -0,0 +1,12 @@
+from cryptography.fernet import Fernet
+
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt(message, key):
+    f = Fernet(key)
+    return f.encrypt(message.encode())
+
+def decrypt(message, key):
+    f = Fernet(key)
+    return f.decrypt(message.decode())
---
+++ main.py
@@ -0,0 +1,16 @@
+import argparse
+from encrypt import generate_key, encrypt, decrypt
+
+parser = argparse.ArgumentParser(description='Encrypt or decrypt a message with AES-256')
+parser.add_argument("--encrypt", metavar="TEKST", help="Text to Encrypt.")
+parser.add_argument("--decrypt", metavar="VERSLEUTELD", help="Text to Decrypt.")
+parser.add_argument("--key", metavar="SLEUTEL", help="Key for Encryption/Decryption.")
+parser.add_argument("--generate-key", action="store_true", help="Generate a new key.")
+
+args = parser.parse_args()
+if args.generate_key:
+    print("New key:", generate_key().decode())
+elif args.encrypt and args.key:
+    print("Encrypted:", encrypt(args.encrypt, args.key.encode()))
+elif args.decrypt and args.key:
+    print("Decrypted:", decrypt(args.decrypt.encode, args.key.encode()))
