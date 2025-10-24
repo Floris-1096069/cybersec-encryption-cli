188a9bdd371d393323ae1ffadef0b4569a1ae3b8
Floris-1096069 <1096069@hr.nl>
2025-10-24 22:56:58 +0200

    Chore, made metavars consistent with rest of app

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pyperclip
 from encrypt import generate_key, encrypt, decrypt
 
 parser = argparse.ArgumentParser(description='Encrypt or decrypt a message with AES-128')
-parser.add_argument("--encrypt", metavar="TEKST", help="Text to Encrypt.")
-parser.add_argument("--decrypt", metavar="VERSLEUTELD", help="Text to Decrypt.")
-parser.add_argument("--key", metavar="SLEUTEL", help="Key for Encryption/Decryption.")
+parser.add_argument("--encrypt", metavar="TEXT", help="Text to Encrypt.")
+parser.add_argument("--decrypt", metavar="ENCRYPTED", help="Text to Decrypt.")
+parser.add_argument("--key", metavar="KEY", help="Key for Encryption/Decryption.")
 parser.add_argument("--generate-key", action="store_true", help="Generate a new key.")
 
 args = parser.parse_args()
